0bc556f4e024e7a02086692f116154e72f0f0f79
Dattobias <152405844+datto2530@users.noreply.github.com>
2023-12-23 12:03:58 +0700

    da xong

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Mang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumList = 0;
+            int[] myList = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("Tong cua mang la");
+            for (int i = 0; i <= myList.Length-1; i++) //for(int i = 0; i < myList.Length; i++)
+            {
+                sumList += myList[i];
+            }
+            Console.WriteLine(sumList);
+            Console.ReadKey();
+        }
+    }
+}
